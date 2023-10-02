755090f751816dca771c2b3180a4d2d98a1a7a50
IT STEP <memasik4you@gmal.com>
2023-10-02 20:36:46 +0300

    Ex2

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-line = input("Введите строку: ")
-line = line.replace(" ", "").upper()
-if line == line[::-1]:
-    print("Палиндром")
-else:
-    print("Не палиндром")
-
-
+text = input("Введіть текст: ")
+reserved_words = input("Ведіть список  зареєстрованих слів: ")
+words = text.split()
+for i in range(len(words)):
+    word = words[i].strip('.,!?').lower()
+    if word in reserved_words:
+        words[i] = words[i].upper()
+        new_text = ' '.join(words)
+        print("Замінений текст:")
+        print(new_text)
